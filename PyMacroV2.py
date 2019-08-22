fe6236602b86e94f1e1a88f9fb71bc3b72c1705c
admin-ll55 <36874774+admin-ll55@users.noreply.github.com>
2019-08-23 06:16:24 +0800

    Add files via upload

---
+++ PyMacroV2.py
@@ -0,0 +1,73 @@
+from math import *
+import pynput
+import time, win32gui, win32con, win32com
+
+_mouse = pynput.mouse.Controller()
+_keyboard = pynput.keyboard.Controller()
+
+def KeyDown(key):
+    _keyboard.press(key)
+
+def KeyUp(key):
+    _keyboard.release(key)
+
+def KeyPress(key):
+    KeyDown(key)
+    Delay(STATIC_DELAY)
+    KeyUp(key)
+
+def MoveMouse(dx, dy):
+    _mouse.position = (dx, dy)
+
+def MouseLDown():
+    _mouse.press(pynput.mouse.Button.left)
+
+def MouseLUp():
+    _mouse.release(pynput.mouse.Button.left)
+
+def MouseLPress(dx, dy):
+    MoveMouse(dx, dy)
+    Delay(STATIC_DELAY)
+    MouseLDown()
+    Delay(STATIC_DELAY)
+    MouseLUp()
+
+def MouseLDrag(dx1, dy1, dx2, dy2):
+    MoveMouse(dx1, dy1)
+    Delay(STATIC_DELAY)
+    MouseLDown()
+    Delay(STATIC_DELAY)
+    MoveMouse(dx2, dy2)
+    Delay(STATIC_DELAY)
+    MouseLUp()
+
+def MouseRDown():
+    _mouse.press(pynput.mouse.Button.right)
+
+def MouseRUp():
+    _mouse.release(pynput.mouse.Button.right)
+
+def MouseRPress(dx, dy):
+    MoveMouse(dx, dy)
+    Delay(STATIC_DELAY)
+    MouseRDown()
+    Delay(STATIC_DELAY)
+    MouseRUp()
+
+def MouseRDrag(dx1, dy1, dx2, dy2):
+    MoveMouse(dx1, dy1)
+    Delay(STATIC_DELAY)
+    MouseRDown()
+    Delay(STATIC_DELAY)
+    MoveMouse(dx2, dy2)
+    Delay(STATIC_DELAY)
+    MouseRUp()
+
+def Delay(sec):
+    time.sleep(sec)
+
+def SwitchToWindow(title):
+    win32gui.ShowWindow(win32gui.FindWindow(None, title), win32con.SW_RESTORE)
+    win32com.client.Dispatch("WScript.Shell").AppActivate(title)
+
+STATIC_DELAY = 0.1
---
+++ PyMacroV2_driver.py
@@ -0,0 +1,37 @@
+# imports
+from pynput.keyboard import *
+from PyMacroV2 import *
+
+# Your macros & normal py codes
+_1 = {"x":680,"y":880}
+
+def macro_1():
+    MouseLPress(_1["x"],_1["y"])
+
+def macro_f1():
+    MouseLPress(_1["x"]*1.1,_1["y"]*1.1)
+
+# Map keys to the corresponding function
+# Value of key at https://pynput.readthedocs.io/en/latest/keyboard.html#pynput.keyboard.Key
+combination_to_function = {
+    frozenset([KeyCode(char='1')]): macro_1, # Pressing key "1" executes "macro_1"
+    frozenset([Key.f1]): macro_f1 # Pressing key "F1" executes "macro_f1"
+}
+
+# Main program
+
+current_keys = set()
+
+def on_press(key):
+    current_keys.add(key)
+    if frozenset(current_keys) in combination_to_function:
+        combination_to_function[frozenset(current_keys)]()
+
+def on_release(key):
+    try:
+        current_keys.remove(key)
+    except:
+        pass
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
