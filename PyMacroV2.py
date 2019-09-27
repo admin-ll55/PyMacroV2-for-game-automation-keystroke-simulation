5853a6f71bf77e32723a95df0f7bff4e6c0ac1f0
admin-ll55 <36874774+admin-ll55@users.noreply.github.com>
2019-09-27 15:25:20 +0800

    updated, pending for example updates

--- PyMacroV2.py
+++ PyMacroV2.py
@@ -8,6 +8,8 @@ import time, win32gui, win32con, win32com.client
 _mouse = pynput.mouse.Controller()
 _keyboard = pynput.keyboard.Controller()
 
+ESC = Key.esc
+
 def Key_(char):
     return KeyCode(char=char)
 
@@ -21,6 +23,7 @@ def KeyPress(key):
     KeyDown(key)
     Delay(STATIC_DELAY)
     KeyUp(key)
+    # print("[debug]pressed "+str(key))
 
 def MoveMouse(dx, dy):
     _mouse.position = (dx, dy)
@@ -37,6 +40,7 @@ def MouseLPress(dx, dy):
     MouseLDown()
     Delay(STATIC_DELAY)
     MouseLUp()
+    # print("[debug]clicked at ("+str(dx)+","+str(dy)+")") # debug
 
 def MouseLDrag(dx1, dy1, dx2, dy2):
     MoveMouse(dx1, dy1)
@@ -59,6 +63,7 @@ def MouseRPress(dx, dy):
     MouseRDown()
     Delay(STATIC_DELAY)
     MouseRUp()
+    # print("[debug]clicked at ("+str(dx)+","+str(dy)+")") # debug
 
 def MouseRDrag(dx1, dy1, dx2, dy2):
     MoveMouse(dx1, dy1)
@@ -72,24 +77,46 @@ def MouseRDrag(dx1, dy1, dx2, dy2):
 def Delay(sec):
     time.sleep(sec)
 
-def IfWindowExists(title):
+def WindowExists(title):
     if win32gui.FindWindow(None, title) == 0:
         return False
     return True
 
 def SwitchToWindow(title):
-    if IfWindowExists(title):
+    if WindowExists(title):
         win32gui.ShowWindow(win32gui.FindWindow(None, title), win32con.SW_RESTORE)
         win32com.client.Dispatch("WScript.Shell").AppActivate(title)
 
 def MoveWindow(title, x, y):
-    if IfWindowExists(title):
+    if WindowExists(title):
         SwitchToWindow(title)
         hwnd = win32gui.FindWindow(None, title)
         rect = win32gui.GetWindowRect(hwnd)
         win32gui.MoveWindow(hwnd, x, y, rect[2]-rect[0], rect[3]-rect[1], True)
 
 def FindImage(fn, x1, y1, x2, y2, precision):
-    return imagesearcharea(fn, x1, y1, x2, y2, precision, region_grabber((x1, y1, x2, y2)))
+    # SaveImage("[debug]prtscr at ("+str(x1)+","+str(y1)+","+str(x2)+","+str(y2)+")",x1,y1,x2,y2) # debug
+    return imagesearcharea(fn, x1, y1, x2, y2, precision)
+
+def SaveImage(fn, x1, y1, x2, y2):
+    im = region_grabber(region=(x1, y1, x2, y2))
+    if is_retina:
+        im.thumbnail((round(im.size[0] * 0.5), round(im.size[1] * 0.5)))
+    im.save(fn+".png")
+
+def ClickOnImage(fn, x1, y1, x2, y2, precision):
+    pos = FindImage(fn, x1, y1, x2, y2, precision)
+    if pos[0] == -1:
+      # print("[debug]"+fn+" not found in ("+str(x1)+","+str(y1)+","+str(x2)+","+str(y2)+")") # debug
+      return False
+    img = cv2.imread(fn)
+    height, width, channels = img.shape
+    # print("[debug]clicked at ("+str(pos[0])+","+str(pos[0])+")") # debug
+    MouseLPress(pos[0]+width/2, pos[1]+height/2)
+    return True
 
 STATIC_DELAY = 0.1
+
+def DictToObject(dict):
+    return type('',(object,),dict)()
+
